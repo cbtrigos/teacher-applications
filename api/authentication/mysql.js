f7082809a65657b6a5b3fb465c1598c086a7a74e
Citlali Trigos <cbtrigos@mit.edu>
2019-12-11 11:30:39 +0000

    environmental vars

---
+++ api/authentication/mysql.js
@@ -0,0 +1,18 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+const db = {
+  "host"     : process.env.DB_HOST,
+  "user"    : process.env.DB_USER,
+  "password" : process.env.DB_PASSWORD,
+  "database": process.env.DB_DATABASE,
+  "insecureAuth" : true
+  }
+
+module.exports = Object.freeze({
+  db: db
+})
+
+
+ 
